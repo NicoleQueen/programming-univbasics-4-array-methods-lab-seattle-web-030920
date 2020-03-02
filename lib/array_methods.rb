0eb3416778038bb2c5ffa628772f953998ebb4b2
QIuyan Peng <nicolepeng99@gmail.com>
2020-03-02 04:47:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,10 @@
 def using_include(array, element)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+ 
   array.include?(element)
 end
 
 def using_sort(array)
-
+  array.sort
 end
 
 def using_reverse(array)
